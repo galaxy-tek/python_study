07e0d931a38d0693af716f6e3b3075a90ba163e4
darkness <haikunxu@126.com>
2017-10-02 09:38:00 +0800

    for loop study

---
+++ for_loop/for_loop.py
@@ -0,0 +1,21 @@
+##
+##for loop from 0 to n
+for i in range(10) :
+	print("current i = ", i)
+##
+##for loop from a to b
+for i in range(-5,5) :
+	print("current i = ", i)
+	
+##
+##for loop for each character in a string
+aString = "Hello World"
+for c in aString :
+	print("current character = ", c)
+
+##
+##for loop for each items in a collection
+fruits = ["apple","banana","strawberry"]
+for fruit in fruits :
+	print("current fruit in fruits = ", fruit)
+	
